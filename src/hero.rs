8507af83f37fe036ded5e3b19ec77aa12f9d8ce4
Jellyfishes72 <jude.f.brooker@gmail.com>
2024-07-07 21:16:34 +0100

    Move hero structures to separate module

---
+++ src/hero.rs
@@ -0,0 +1,111 @@
+use std::fmt::Display;
+
+use serde::{Deserialize, Serialize};
+
+const TANKS: [&str; 12] = [
+    "D.va",
+    "Doomfist",
+    "Junker Queen",
+    "Mauga",
+    "Orisa",
+    "Ramatra",
+    "Reinhardt",
+    "Roadhog",
+    "Sigma",
+    "Winston",
+    "Wrecking Ball",
+    "Zarya",
+];
+
+const DAMAGES: [&str; 17] = [
+    "Ashe",
+    "Bastion",
+    "Cassidy",
+    "Echo",
+    "Genji",
+    "Hanzo",
+    "Junkrat",
+    "Mei",
+    "Pharah",
+    "Reaper",
+    "Sojourn",
+    "Soldier: 76",
+    "Sombra",
+    "Symmetra",
+    "Törbjorn",
+    "Venture",
+    "Widowmaker",
+];
+
+const SUPPORTS: [&str; 10] = [
+    "Ana",
+    "Baptiste",
+    "Brigitte",
+    "Illari",
+    "Kiriko",
+    "Lifeweaver",
+    "Lúcio",
+    "Mercy",
+    "Moira",
+    "Zenyatta",
+];
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Heroes {
+    pub tanks: Vec<Hero>,
+    pub damages: Vec<Hero>,
+    pub supports: Vec<Hero>,
+}
+
+impl Default for Heroes {
+    fn default() -> Self {
+        let mut tanks = Vec::new();
+        for tank in TANKS {
+            tanks.push(Hero::new(tank))
+        }
+        let mut damages = Vec::new();
+        for damage in DAMAGES {
+            damages.push(Hero::new(damage))
+        }
+        let mut supports = Vec::new();
+        for support in SUPPORTS {
+            supports.push(Hero::new(support))
+        }
+        Self {
+            tanks,
+            damages,
+            supports,
+        }
+    }
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Hero {
+    pub name: String,
+    pub level: u32,
+    pub favourite: bool,
+}
+
+impl Hero {
+    pub fn new(name: &str) -> Self {
+        Self {
+            name: name.to_string(),
+            level: 1,
+            favourite: false,
+        }
+    }
+
+    pub fn level_up(&mut self) {
+        self.level += 1;
+    }
+
+    pub fn toggle_favourite(&mut self) {
+        self.favourite = !self.favourite;
+    }
+}
+
+impl Display for Hero {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} - {}", self.level, self.name)
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use eframe::egui::{self, Color32, RichText};
+use hero::*;
 use rand::prelude::SliceRandom;
-use serde::{Deserialize, Serialize};
-use std::{fmt::Display, fs::File, path::Path};
+use std::{fs::File, path::Path};
+
+mod hero;
 
 const KEY_TANK: &str = "tank";
 const KEY_DAMAGE: &str = "damage";
@@ -10,114 +12,6 @@ const KEY_FAVOURITE: &str = "favourite";
 const KEY_LOWEST: &str = "lowest";
 
 const HEROES_FILE_PATH: &str = "heroes.yaml";
-
-const TANKS: [&str; 12] = [
-    "D.va",
-    "Doomfist",
-    "Junker Queen",
-    "Mauga",
-    "Orisa",
-    "Ramatra",
-    "Reinhardt",
-    "Roadhog",
-    "Sigma",
-    "Winston",
-    "Wrecking Ball",
-    "Zarya",
-];
-
-const DAMAGES: [&str; 17] = [
-    "Ashe",
-    "Bastion",
-    "Cassidy",
-    "Echo",
-    "Genji",
-    "Hanzo",
-    "Junkrat",
-    "Mei",
-    "Pharah",
-    "Reaper",
-    "Sojourn",
-    "Soldier: 76",
-    "Sombra",
-    "Symmetra",
-    "Törbjorn",
-    "Venture",
-    "Widowmaker",
-];
-
-const SUPPORTS: [&str; 10] = [
-    "Ana",
-    "Baptiste",
-    "Brigitte",
-    "Illari",
-    "Kiriko",
-    "Lifeweaver",
-    "Lúcio",
-    "Mercy",
-    "Moira",
-    "Zenyatta",
-];
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Heroes {
-    pub tanks: Vec<Hero>,
-    pub damages: Vec<Hero>,
-    pub supports: Vec<Hero>,
-}
-
-impl Default for Heroes {
-    fn default() -> Self {
-        let mut tanks = Vec::new();
-        for tank in TANKS {
-            tanks.push(Hero::new(tank))
-        }
-        let mut damages = Vec::new();
-        for damage in DAMAGES {
-            damages.push(Hero::new(damage))
-        }
-        let mut supports = Vec::new();
-        for support in SUPPORTS {
-            supports.push(Hero::new(support))
-        }
-        Self {
-            tanks,
-            damages,
-            supports,
-        }
-    }
-}
-
-#[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Hero {
-    pub name: String,
-    pub level: u32,
-    pub favourite: bool,
-}
-
-impl Hero {
-    pub fn new(name: &str) -> Self {
-        Self {
-            name: name.to_string(),
-            level: 1,
-            favourite: false,
-        }
-    }
-
-    pub fn level_up(&mut self) {
-        self.level += 1;
-    }
-
-    pub fn toggle_favourite(&mut self) {
-        self.favourite = !self.favourite;
-    }
-}
-
-impl Display for Hero {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{} - {}", self.level, self.name)
-    }
-}
 
 fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
